36b16b28c31cf3962394177a7e5e16447f80d33b
AliAais <aaisali228@gmail.com>
2024-05-24 09:34:05 -0700

    comment

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -5,9 +5,9 @@ import React from 'react';
 const Dashboard = () => {
     const {userName}=useParams();
     const [user,setUser]=React.useState('');
-    const getUser=async()=>{
-        const resp=axios.get('http://localhost:3000/auth/user')
-    }
+    // const getUser=async()=>{
+    //     const resp=axios.get('http://localhost:3000/auth/user')
+    // }
   return (
     <div>
         <div>Hello my Lovely <strong>{userName}</strong></div>
